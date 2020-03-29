63598dbe80c4999cfb8646222aa413f6ea80a5d6
Chris Rasys <chris.rasys@gmail.com>
2020-03-29 02:45:37 -0400

    Add a custom color wrapper class
    This will undoubtably be re-worked at some point, but will suffice for now.

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -19,3 +19,20 @@ app.use('/', router);
 app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
 })
+
+class BSColor {
+    constructor(input) {
+        this.ratioValue = 16777215;
+        this.color = Color(input);
+    }
+
+    set nodeValue(input) {
+        var newColor = input * this.ratioValue;
+        this.color = Color(newColor);
+    }
+
+    get nodeValue() {
+        if (this.color.rgbNumber() == 0) return 0;
+        return  this.color.rgbNumber() / this.ratioValue;
+    }
+}
