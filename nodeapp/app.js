f246b43c86c128e1ee5d208b73bb3bc8812eee0d
Chris Rasys <chris.rasys@gmail.com>
2020-05-07 14:59:02 -0400

    Move userinfo collector to new class
    Will need to break this out into a new file soon

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -39,53 +39,9 @@ router.use(function (req,res,next) {
 });
   
 function collectUserInfo(req) {
-    var userInfo = {};
-    var ua = uaParser(req.headers['user-agent']);
-    var languages = languageParser.parse(req.headers['accept-language']);
     
-    userInfo.ip = req.ip;
-    var geo = geoip.lookup(userInfo.ip);
-    
-
-    if ( (ua !== null) && (ua !== undefined)) {
-        userInfo.browserName = ua.browser.name;
-        userInfo.browserMajorVersion = ua.browser.major;
-        userInfo.browserVersion = ua.browser.version;
-        userInfo.osName = ua.os.name;
-        userInfo.osVersion = ua.os.version;
-        userInfo.cpuArchitecture = ua.cpu.architecture;
     }
 
-    if ( (geo !== null) && (geo !== undefined )) {
-        userInfo.country = geo.country;
-        userInfo.region = geo.region;
-        // look up what geo.eu is
-        userInfo.timezone = geo.timezone;
-        userInfo.city = geo.city;
-        userInfo.latitude = geo.ll[0];
-        userInfo.longitude = geo.ll[1];
-        userInfo.metro = geo.metro;
-        userInfo.area = geo.area;
-    }
-
-    if ( (languages !== null) && (languages !== undefined)) {
-        // We're only going to deal with the top result
-        userInfo.languageCode = languages[0].code;
-        userInfo.languageRegion = languages[0].region;
-    }
-
-    if (userInfo.timezone !== undefined) {
-        var currentTime = moment().tz(userInfo.timezone);
-        userInfo.day = currentTime.format('DD');
-        userInfo.month = currentTime.format('MM');
-        userInfo.year = currentTime.format('YYYY');
-        userInfo.hour = currentTime.format('HH');
-        userInfo.minute = currentTime.format('mm');
-    }
-
-    return userInfo;
-}
-  
 router.get('/', function(req,res){
     res.sendFile(path + 'index.html');
 });
@@ -166,6 +122,51 @@ app.listen(port, function () {
     console.log('Example app listening on port '+port+'!');
 })
 
+class BSUser {
+    constructor(req) {
+        var ua = uaParser(req.headers['user-agent']);
+        var languages = languageParser.parse(req.headers['accept-language']);
+        
+        this.ip = req.ip;
+        var geo = geoip.lookup(this.ip);
+
+        if ( (ua !== null) && (ua !== undefined)) {
+            this.browserName = ua.browser.name;
+            this.browserMajorVersion = ua.browser.major;
+            this.browserVersion = ua.browser.version;
+            this.osName = ua.os.name;
+            this.osVersion = ua.os.version;
+            this.cpuArchitecture = ua.cpu.architecture;
+        }
+
+        if ( (geo !== null) && (geo !== undefined )) {
+            this.country = geo.country;
+            this.region = geo.region;
+            // look up what geo.eu is
+            this.timezone = geo.timezone;
+            this.city = geo.city;
+            this.latitude = geo.ll[0];
+            this.longitude = geo.ll[1];
+            this.metro = geo.metro;
+            this.area = geo.area;
+        }
+
+        if ( (languages !== null) && (languages !== undefined)) {
+            // We're only going to deal with the top result
+            this.languageCode = languages[0].code;
+            this.languageRegion = languages[0].region;
+        }
+
+        if (this.timezone !== undefined) {
+            var currentTime = moment().tz(this.timezone);
+            this.day = currentTime.format('DD');
+            this.month = currentTime.format('MM');
+            this.year = currentTime.format('YYYY');
+            this.hour = currentTime.format('HH');
+            this.minute = currentTime.format('mm');
+        }
+    }
+}
 class BSColor {
     constructor(input) {
         this.ratioValue = 16777215;
