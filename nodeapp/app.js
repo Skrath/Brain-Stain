bc7d46dcee9b09a920f34806c5d6d08b85386461
Chris Rasys <chris.rasys@gmail.com>
2020-05-08 00:14:22 -0400

    Move BSUser class into seperate file

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -23,10 +23,8 @@ app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
 var hbs = require('hbs');
-var geoip = require('geoip-lite');
-var uaParser = require('ua-parser-js');
-var languageParser = require('accept-language-parser');
-var moment = require('moment-timezone');
+
+var BSUser = require("./classes/BSUser.class.js");
 
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
@@ -38,10 +36,6 @@ router.use(function (req,res,next) {
     next();
 });
   
-function collectUserInfo(req) {
-    
-    }
-
 router.get('/', function(req,res){
     res.sendFile(path + 'index.html');
 });
@@ -122,51 +116,6 @@ app.listen(port, function () {
     console.log('Example app listening on port '+port+'!');
 })
 
-class BSUser {
-    constructor(req) {
-        var ua = uaParser(req.headers['user-agent']);
-        var languages = languageParser.parse(req.headers['accept-language']);
-        
-        this.ip = req.ip;
-        var geo = geoip.lookup(this.ip);
-
-        if ( (ua !== null) && (ua !== undefined)) {
-            this.browserName = ua.browser.name;
-            this.browserMajorVersion = ua.browser.major;
-            this.browserVersion = ua.browser.version;
-            this.osName = ua.os.name;
-            this.osVersion = ua.os.version;
-            this.cpuArchitecture = ua.cpu.architecture;
-        }
-
-        if ( (geo !== null) && (geo !== undefined )) {
-            this.country = geo.country;
-            this.region = geo.region;
-            // look up what geo.eu is
-            this.timezone = geo.timezone;
-            this.city = geo.city;
-            this.latitude = geo.ll[0];
-            this.longitude = geo.ll[1];
-            this.metro = geo.metro;
-            this.area = geo.area;
-        }
-
-        if ( (languages !== null) && (languages !== undefined)) {
-            // We're only going to deal with the top result
-            this.languageCode = languages[0].code;
-            this.languageRegion = languages[0].region;
-        }
-
-        if (this.timezone !== undefined) {
-            var currentTime = moment().tz(this.timezone);
-            this.day = currentTime.format('DD');
-            this.month = currentTime.format('MM');
-            this.year = currentTime.format('YYYY');
-            this.hour = currentTime.format('HH');
-            this.minute = currentTime.format('mm');
-        }
-    }
-}
 class BSColor {
     constructor(input) {
         this.ratioValue = 16777215;
---
+++ nodeapp/classes/BSUser.class.js
@@ -0,0 +1,50 @@
+var geoip = require('geoip-lite');
+var uaParser = require('ua-parser-js');
+var languageParser = require('accept-language-parser');
+var moment = require('moment-timezone');
+
+module.exports = class BSUser {
+    constructor(req) {
+        var ua = uaParser(req.headers['user-agent']);
+        var languages = languageParser.parse(req.headers['accept-language']);
+        
+        this.ip = req.ip;
+        var geo = geoip.lookup(this.ip);
+
+        if ( (ua !== null) && (ua !== undefined)) {
+            this.browserName = ua.browser.name;
+            this.browserMajorVersion = ua.browser.major;
+            this.browserVersion = ua.browser.version;
+            this.osName = ua.os.name;
+            this.osVersion = ua.os.version;
+            this.cpuArchitecture = ua.cpu.architecture;
+        }
+
+        if ( (geo !== null) && (geo !== undefined )) {
+            this.country = geo.country;
+            this.region = geo.region;
+            // look up what geo.eu is
+            this.timezone = geo.timezone;
+            this.city = geo.city;
+            this.latitude = geo.ll[0];
+            this.longitude = geo.ll[1];
+            this.metro = geo.metro;
+            this.area = geo.area;
+        }
+
+        if ( (languages !== null) && (languages !== undefined)) {
+            // We're only going to deal with the top result
+            this.languageCode = languages[0].code;
+            this.languageRegion = languages[0].region;
+        }
+
+        if (this.timezone !== undefined) {
+            var currentTime = moment().tz(this.timezone);
+            this.day = currentTime.format('DD');
+            this.month = currentTime.format('MM');
+            this.year = currentTime.format('YYYY');
+            this.hour = currentTime.format('HH');
+            this.minute = currentTime.format('mm');
+        }
+    }
+};
